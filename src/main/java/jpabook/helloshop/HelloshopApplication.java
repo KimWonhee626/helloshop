e9dec8168779e3032045986323dcb3d69f10017c
KimWonhee626 <kwh7739@gmail.com>
2022-08-26 16:00:46 +0900

    First commit

---
+++ src/main/java/jpabook/helloshop/HelloshopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.helloshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloshopApplication.class, args);
+	}
+
+}
